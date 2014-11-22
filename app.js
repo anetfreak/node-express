5f51379251cf058324e855da77b06c42461e8b3a
Ameya <ameyaajay.joshi@sjsu.edu>
2014-11-22 13:16:11 -0800

    Wired up all the stuff..

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ app.use(stylus.middleware(
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function (req, res) {
-  res.end('Wassup Yo!')
+  res.render('index',
+  { title : 'Home' }
+  )
 })
 app.listen(3000)
